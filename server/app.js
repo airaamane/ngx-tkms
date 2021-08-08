a6dd9b42131862cb0fdc37915f74fb678b6d0a00
Iraamane, Abdellah <abdellah.airaamane@gmail.com>
2021-08-08 15:29:48 +0100

    feat(state): added store and actions

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
-let taxis = require("./data/taxis");
 const cors = require('cors')
+let taxis = require("./data/taxis");
+
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(cors());
@@ -45,5 +46,5 @@ app.patch("/taxis", function (req, res, next) {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`TKMS server listening at http://localhost:${port}`);
 });
--- src/app/taxi-page/taxi-page.component.ts
+++ src/app/taxi-page/taxi-page.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { TaxiModel, TaxiModelRequired } from '../config/taxi.model';
 import { TaxiService } from '../services/taxi.service';
+import { TaxiAPIActions } from '../state/taxis-api.actions';
+import { TaxiPageActions } from '../state/taxis.actions';
+import { AppState } from '../state/taxis.state';
 
 @Component({
   selector: 'app-taxi-page',
@@ -13,34 +17,50 @@ export class TaxiPageComponent implements OnInit {
   public taxis$: Observable<TaxiModel[]>;
   public selectedTaxi: TaxiModel | null = null;
 
-  constructor(private taxiService: TaxiService) {}
+  constructor(
+    private taxiService: TaxiService,
+    private store: Store<AppState>
+  ) {}
 
   ngOnInit(): void {
+    this.store.dispatch(TaxiPageActions.enter());
     this.getTaxis();
   }
 
+  public getTaxis(): void {
+    this.taxis$ = this.taxiService
+      .all()
+      .pipe(
+        tap((taxis) =>
+          this.store.dispatch(TaxiAPIActions.fetchTaxisSuccess({ taxis }))
+        )
+      );
+  }
+
+  public add(taxi: TaxiModelRequired): void {
+    this.store.dispatch(TaxiPageActions.addTaxi({ taxi }));
+    this.taxiService.add(taxi).pipe(
+      tap((taxiAdded) =>
+        this.store.dispatch(TaxiAPIActions.addTaxiSuccess({ taxi: taxiAdded }))
+      ),
+      tap(() => this.clear()),
+      tap(() => this.getTaxis())
+    ).subscribe();
+  }
+
   public select(taxi: TaxiModel | null): void {
+    this.store.dispatch(
+      TaxiPageActions.selectTaxi({ taxiId: taxi?._id ?? null })
+    );
+
     this.taxiService
       .oneById(taxi._id)
       .subscribe((res) => (this.selectedTaxi = res));
   }
 
-  public getTaxis(): void {
-    this.taxis$ = this.taxiService.all();
-  }
-
   public clear(): void {
+    this.store.dispatch(TaxiPageActions.clearSelctedTaxi());
     this.selectedTaxi = null;
-  }
-
-  public add(taxi: TaxiModelRequired): void {
-    this.taxiService
-      .add(taxi)
-      .pipe(
-        tap(() => this.clear()),
-        tap(() => this.getTaxis())
-      )
-      .subscribe();
   }
 
   public edit(taxi: TaxiModel): void {
@@ -51,12 +71,9 @@ export class TaxiPageComponent implements OnInit {
   }
 
   public remove(id: string): void {
-    this.taxiService
-      .delete(id)
-      .pipe(
-        tap(() => this.clear()),
-        tap(() => this.getTaxis())
-      )
-      .subscribe();
+    this.taxiService.delete(id).pipe(
+      tap(() => this.clear()),
+      tap(() => this.getTaxis())
+    ).subscribe();
   }
 }
